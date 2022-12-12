2c84e6a0d0f5cf795c3be46b4b08349ae78a2f46
mattialicausi <licausivitomattia@gmail.com>
2022-12-12 17:15:24 +0100

    Stampato in layout i due oggetti

--- index.php
+++ index.php
@@ -43,3 +43,94 @@ var_dump($movie2);
 echo 'Il film è: ' . $movie2->popular;
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js" defer></script>
+    <title>OOP</title>
+</head>
+<body>
+    <header>
+        <h1 class="text-center p-4">
+            Movies
+        </h1>
+    </header>
+
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-6">
+                    <img class="img-fluid cover-film" src="<?php echo $movie1->image ?>" alt="<?php echo $movie1->title ?>">
+                    <div class="container_card">
+                        <h3>
+                            <?php echo $movie1->title ?>
+                        </h3>
+                        <div>
+                            <?php echo $movie1->genres->genres1 ?>
+                        </div>
+                        <div>
+                            <?php echo $movie1->description ?>
+                        </div>
+                        <div>
+                            Uscito il: <?php echo $movie1->releaseDate ?>
+                        </div>
+                        <div class="container_cast p-4">
+                            <img class="img-fluid img-cast" src="<?php echo $movie1->cast->image ?>" alt="<?php echo $movie1->title ?>">
+                            <div class="container-info">
+                                <div>Name: <?php echo $movie1->cast->name ?></div>
+                                <div>Surname: <?php echo $movie1->cast->surname ?></div>
+                                <div><?php echo $movie1->cast->yearsOld ?> years old</div>
+                                <div>Role: <?php echo $movie1->cast->role ?></div>
+                                <div>Nationality: <?php echo $movie1->cast->nationality ?></div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="col-6">
+                    <img class="img-fluid cover-film" src="<?php echo $movie2->image ?>" alt="<?php echo $movie2->title ?>">
+                    <div class="container_card">
+                        <h3>
+                            <?php echo $movie2->title ?>
+                        </h3>
+                        <div>
+                            <?php echo $movie2->genres->genres1 ?>
+                        </div>
+                        <div>
+                            <?php echo $movie2->description ?>
+                        </div>
+                        <div>
+                            Uscito il: <?php echo $movie2->releaseDate ?>
+                        </div>
+                        <div class="container_cast p-4">
+                            <img class="img-fluid img-cast" src="<?php echo $movie2->cast->image ?>" alt="<?php echo $movie2->title ?>">
+                            <div class="container-info">
+                                <div>Name: <?php echo $movie2->cast->name ?></div>
+                                <div>Surname: <?php echo $movie2->cast->surname ?></div>
+                                <div><?php echo $movie2->cast->yearsOld ?> years old</div>
+                                <div>Role: <?php echo $movie2->cast->role ?></div>
+                                <div>Nationality: <?php echo $movie2->cast->nationality ?></div>
+                            </div>
+
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <footer class="text-center p-4">
+        Made by Mattia Li Causi
+    </footer>
+</body>
+</html>
