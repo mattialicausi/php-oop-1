f807ad4f2463b04e5cb9bbc32006f50d8ff09dfd
mattialicausi <licausivitomattia@gmail.com>
2022-12-12 14:21:13 +0100

    preparo repo e aggiungo variabili in Movie.php

---
+++ Models/Movie.php
@@ -0,0 +1,22 @@
+<?php
+
+class Movie {
+
+    // variabili
+    public $title;
+    public $genres;
+    public $description;
+ 
+    public $releaseYear;
+    public $image;
+    public $video;
+
+
+    // variabili appartenenti ad altre classi
+    public $cast;
+}
+
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+// includo Movie.php nella index
+include_once __DIR__ . '/Models/Movie.php';
+
+
+?>
